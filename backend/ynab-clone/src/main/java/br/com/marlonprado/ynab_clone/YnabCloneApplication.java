259b79dd0604dd5cdecabbd3e43f16dc0ad4badf
Marlon Prado - Windows <marlonprado04@gmail.com>
2025-05-28 23:18:39 -0300

    feat: added initial proj spring boot o/

---
+++ backend/ynab-clone/src/main/java/br/com/marlonprado/ynab_clone/YnabCloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.marlonprado.ynab_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnabCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnabCloneApplication.class, args);
+	}
+
+}
