dcf3cc35266457cf80410c989b6c933db436d13d
Marlon Prado - Windows <marlonprado04@gmail.com>
2025-06-09 00:11:49 -0300

    feat: added backend initial folders

---
+++ backend/src/main/java/br/com/marlonprado/ynab_clone_api/YnabCloneApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marlonprado.ynab_clone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnabCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnabCloneApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/marlonprado/ynab_clone_api/YnabCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonprado.ynab_clone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnabCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
